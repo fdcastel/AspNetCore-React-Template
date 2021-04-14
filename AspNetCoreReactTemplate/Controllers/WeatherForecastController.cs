dec9e4aa2f12bc9f5480b679414c17a4779655a1
F.D.Castel <fdcastel@gmail.com>
2021-04-14 10:04:18 -0300

    Adds .editorconfig.

--- AspNetCoreReactTemplate/Controllers/WeatherForecastController.cs
+++ AspNetCoreReactTemplate/Controllers/WeatherForecastController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -26,6 +25,8 @@ namespace AspNetCoreReactTemplate.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Getting weather forecast.");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
